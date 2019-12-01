94f73ce79147ef93453dab6b1c5f165fe513d43c
Arnavion <arnavion@gmail.com>
2019-12-01 14:07:22 -0800

    Day 1

---
+++ src/day1.rs
@@ -0,0 +1,60 @@
+pub(super) fn run() -> Result<(), super::Error> {
+	{
+		let result =
+			super::read_input_lines::<u64>("day1")?
+			.try_fold(0, |prev, curr| -> Result<_, super::Error> { Ok(prev + calculate_fuel(curr?)) })?;
+
+		println!("1a: {}", result);
+
+		assert_eq!(result, 3318632);
+	}
+
+	{
+		let result =
+			super::read_input_lines::<u64>("day1")?
+			.try_fold(0, |prev, curr| -> Result<_, super::Error> { Ok(prev + calculate_fuel_recursive(curr?)) })?;
+
+		println!("1b: {}", result);
+
+		assert_eq!(result, 4975084);
+	}
+
+	Ok(())
+}
+
+fn calculate_fuel(mass: u64) -> u64 {
+	(mass / 3).saturating_sub(2)
+}
+
+fn calculate_fuel_recursive(mut mass: u64) -> u64 {
+	let mut result = 0;
+
+	loop {
+		let fuel = calculate_fuel(mass);
+		if fuel == 0 {
+			break;
+		}
+
+		result += fuel;
+		mass = fuel;
+	}
+
+	result
+}
+
+mod tests {
+	#[test]
+	fn calculate_fuel() {
+		assert_eq!(super::calculate_fuel(12), 2);
+		assert_eq!(super::calculate_fuel(14), 2);
+		assert_eq!(super::calculate_fuel(1969), 654);
+		assert_eq!(super::calculate_fuel(100756), 33583);
+	}
+
+	#[test]
+	fn calculate_fuel_recursive() {
+		assert_eq!(super::calculate_fuel_recursive(14), 2);
+		assert_eq!(super::calculate_fuel_recursive(1969), 966);
+		assert_eq!(super::calculate_fuel_recursive(100756), 50346);
+	}
+}
---
+++ src/main.rs
@@ -0,0 +1,132 @@
+#![deny(rust_2018_idioms, warnings)]
+#![deny(clippy::all, clippy::pedantic)]
+#![allow(
+	clippy::default_trait_access,
+	clippy::unreadable_literal,
+)]
+
+macro_rules! main {
+	($($mod:ident ,)*) => {
+		main! {
+			@inner
+			{}
+			{}
+			[$($mod,)*]
+		}
+	};
+
+	(@inner { $($mods:tt)* } { $($calls:tt)* } []) => {
+		$($mods)*
+
+		fn main() -> Result<(), Error> {
+			$($calls)*
+
+			Ok(())
+		}
+	};
+
+	(@inner { $($mods:tt)* } { $($calls:tt)* } [$first:ident , $($rest:ident ,)*]) => {
+		main! {
+			@inner
+			{ $($mods)* mod $first; }
+			{ $($calls)* $first::run()?; }
+			[$($rest ,)*]
+		}
+	}
+}
+
+main! {
+	day1,
+}
+
+fn open_input(filename: &str) -> Result<std::io::BufReader<std::fs::File>, Error> {
+	let mut path = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).to_owned();
+	path.push("inputs");
+	path.push(filename);
+	Ok(std::io::BufReader::new(std::fs::File::open(path)?))
+}
+
+fn read_input_lines<T>(filename: &str) -> Result<impl Iterator<Item = Result<T, Error>>, Error> where T: std::str::FromStr, <T as std::str::FromStr>::Err: Into<Error> {
+	let inner = open_input(filename)?;
+	Ok(Lines::new(inner))
+}
+
+struct Error(Box<dyn std::error::Error>, backtrace::Backtrace);
+
+impl std::fmt::Debug for Error {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		writeln!(f, "{}", self.0)?;
+
+		let mut source = self.0.source();
+		while let Some(err) = source {
+			writeln!(f, "caused by: {}", err)?;
+			source = err.source();
+		}
+
+		writeln!(f)?;
+
+		writeln!(f, "{:?}", self.1)?;
+
+		Ok(())
+	}
+}
+
+impl<E> From<E> for Error where E: Into<Box<dyn std::error::Error>> {
+	fn from(err: E) -> Self {
+		Error(err.into(), Default::default())
+	}
+}
+
+struct Lines<T> {
+	inner: std::io::BufReader<std::fs::File>,
+	buf: String,
+	skipping_header: bool,
+	_ty: std::marker::PhantomData<fn() -> T>,
+}
+
+impl<T> Lines<T> {
+	fn new(inner: std::io::BufReader<std::fs::File>) -> Self {
+		Lines {
+			inner,
+			buf: String::new(),
+			skipping_header: true,
+			_ty: Default::default(),
+		}
+	}
+}
+
+impl<T> Iterator for Lines<T> where T: std::str::FromStr, <T as std::str::FromStr>::Err: Into<Error> {
+	type Item = Result<T, Error>;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		use std::io::BufRead;
+
+		self.buf.clear();
+
+		let read = match self.inner.read_line(&mut self.buf) {
+			Ok(read) => read,
+			Err(err) => return Some(Err(err.into())),
+		};
+		if read == 0 {
+			return None;
+		}
+
+		if self.skipping_header {
+			if self.buf.starts_with('#') {
+				return self.next();
+			}
+			else {
+				self.skipping_header = false;
+			}
+		}
+
+		let buf = self.buf.trim_end();
+
+		let value: T = match buf.parse() {
+			Ok(value) => value,
+			Err(err) => return Some(Err(err.into())),
+		};
+
+		Some(Ok(value))
+	}
+}
